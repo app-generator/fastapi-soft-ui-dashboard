6d2776f2f72a1668092031af71738a44295ffa27
barreraalexander <barreraalexander93@gmail.com>
2022-09-28 15:54:42 -0400

    hourly update

--- src/config.py
+++ src/config.py
@@ -11,7 +11,7 @@ class Settings(BaseSettings):
     access_token_expire_minutes: int
 
     class Config:
-        env_file = ".env"
+        env_file = "../.env"
 
 
 settings = Settings()
--- src/helpers/database.py
+++ src/helpers/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config import settings
 
-SQLALCHEMY_DATABASE_URL = f'sqlite://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
