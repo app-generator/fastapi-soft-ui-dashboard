81ede50d8a2fa51ca8518808567e129981ff1af7
barreraalexander <barreraalexander93@gmail.com>
2022-09-28 16:25:23 -0400

    update to models, hourly update

--- src/app.py
+++ src/app.py
@@ -1,5 +1,8 @@
-from imp import reload
 from fastapi import FastAPI
+
 from helpers.database import engine
+import models
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(debug=True, reload=True)
--- src/models.py
+++ src/models.py
@@ -1,3 +1,24 @@
+from helpers.database import Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
+from sqlalchemy.orm import relationship
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    username = Column(String, nullable=False, unique=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+
+    def __str__(self):
+        return f"""
+        id: {self.id}
+        email: {self.email}
+        username: {self.username}
+        """
+
 # products - id, name, description, price, currency
 # sales - id, product_id, state, value, fee, currency, client
 
@@ -5,3 +26,5 @@
 
 # class Product(Base):
     # pass
+
+    
