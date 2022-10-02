e1fcf39d9bd74cd7dd50ce174896e1dd8770a9c0
barreraalexander <barreraalexander93@gmail.com>
2022-10-01 20:15:21 -0400

    changes before stylistic update

--- src/config.py
+++ src/config.py
@@ -1,17 +1,18 @@
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    database_hostname: str
-    database_port: int
-    database_password: str
-    database_name: str
-    database_username: str
-    secret_key: str
-    algorithm: str
-    access_token_expire_minutes: int
+    database_hostname: str = 'test'
+    database_port: int = 0
+    database_password: str = 'test'
+    database_name: str = 'test'
+    database_username: str = 'test'
+    secret_key: str = 'test'
+    algorithm: str = 'test'
+    access_token_expire_minutes: int = 30
 
     class Config:
         env_file = "../.env"
+        # env_file = "./.env"
 
 
 settings = Settings()
