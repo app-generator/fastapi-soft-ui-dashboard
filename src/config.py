54f688fe2871a7487cb08d6bafba5b4cae8bf485
barreraalexander <barreraalexander93@gmail.com>
2022-10-01 20:44:15 -0400

    rearranged

--- src/config.py
+++ src/config.py
@@ -1,18 +1,17 @@
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    database_hostname: str = 'test'
-    database_port: int = 0
-    database_password: str = 'test'
-    database_name: str = 'test'
-    database_username: str = 'test'
-    secret_key: str = 'test'
-    algorithm: str = 'test'
+    database_hostname: str
+    database_port: int
+    database_password: str
+    database_name: str
+    database_username: str
+    secret_key: str
+    algorithm: str
     access_token_expire_minutes: int = 30
 
     class Config:
-        env_file = "../.env"
-        # env_file = "./.env"
+        env_file = "./.env"
 
 
 settings = Settings()
