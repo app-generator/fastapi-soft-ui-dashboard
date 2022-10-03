8761bcd1559a58e232e98502112eb5f14da870a8
barreraalexander <barreraalexander93@gmail.com>
2022-10-03 09:32:40 -0400

    adding pytest feature - incomplete

---
+++ tests/database.py
@@ -0,0 +1,12 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+from src.app.database import get_db
+from src.app.database import Base
+from src.app.main import app
+from src.app.config import settings
+from src.app.oauth2 import create_access_token
+from src.app import models
