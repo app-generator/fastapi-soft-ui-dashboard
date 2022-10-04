8ff4bfa9dd8e3589cebf72ac9ec17b69eeb68694
barreraalexander <barreraalexander93@gmail.com>
2022-10-04 09:53:21 -0400

    line 17 opens up the api to the public

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.routers.auth.auth_routes import router as auth_router
 from src.routers.product_routes import router as product_router
@@ -6,6 +7,15 @@ from src.routers.user_routes import router as user_router
 from src.routers.sales_routes import router as sales_router
 
 app = FastAPI(debug=True, reload=True)
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins
+)
 
 app.include_router(auth_router)
 app.include_router(user_router)
