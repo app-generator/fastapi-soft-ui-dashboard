d4b35a63dfba211597f49a431008a34e68e21e2b
barreraalexander <barreraalexander93@gmail.com>
2022-10-06 16:26:55 -0400

    loading up templates on root path

--- src/app.py
+++ src/app.py
@@ -1,5 +1,10 @@
-from fastapi import FastAPI
+from re import TEMPLATE
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
+from pathlib import Path
 
 from src.routers.auth.auth_routes import router as auth_router
 from src.routers.product_routes import router as product_router
@@ -16,6 +21,16 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins = origins
 )
+
+BASE_PATH = Path(__file__).resolve().parent
+# TEMPLATES = Jinja2Templates(directory="./src/templates/home")
+TEMPLATES = Jinja2Templates(directory=str(BASE_PATH / "templates"))
+
+
+@app.get("/")
+def home(request: Request):
+    return TEMPLATES.TemplateResponse("home/index.html", {"request" : request})
+
 
 app.include_router(auth_router, prefix='/api')
 app.include_router(user_router, prefix='/api')
