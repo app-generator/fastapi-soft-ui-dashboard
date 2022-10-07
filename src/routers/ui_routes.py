0f5449acf6fd760a0b60f4d593ff958babf0193d
barreraalexander <barreraalexander93@gmail.com>
2022-10-07 13:56:27 -0400

    before adding login manager

--- src/routers/ui_routes.py
+++ src/routers/ui_routes.py
@@ -2,7 +2,11 @@ from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
+import requests
+from src.config import Settings
 
+from src import models
+from src import app
 # from app import TEMPLATES
 
 router = APIRouter(
@@ -15,21 +19,12 @@ TEMPLATES = Jinja2Templates(directory=str(BASE_PATH / "../templates"))
 
 @router.get("/")
 def home(request: Request, response_model=HTMLResponse):
-
     return TEMPLATES.TemplateResponse("home/index.html", {"request" : request})
 
 @router.get("/login")
-def login(request: Request, response_model=HTMLResponse):
+def signin(request: Request, response_model=HTMLResponse):
     return TEMPLATES.TemplateResponse("accounts/login.html", {"request" : request})
 
 @router.get("/register")
 def register(request: Request, response_model=HTMLResponse):
-    return TEMPLATES.TemplateResponse("accounts/register.html", {"request" : request})
-
-@router.post("/register")
-async def register(request: Request):
-    data = await request.form()
-    print (data)
-    
-
     return TEMPLATES.TemplateResponse("accounts/register.html", {"request" : request})
