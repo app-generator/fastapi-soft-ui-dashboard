cd1699f26d6d28d0581e7b927c04e048d80b5701
barreraalexander <barreraalexander93@gmail.com>
2022-10-10 15:09:21 -0400

    update

--- src/routers/ui_routes.py
+++ src/routers/ui_routes.py
@@ -1,13 +1,14 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends, status, Response, HTTPException
+from fastapi.responses import RedirectResponse
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
-import requests
-from src.config import Settings
 
+import src.oauth2 as oauth2
+from src.config import Settings
 from src import models
 from src import app
-# from app import TEMPLATES
+
 
 router = APIRouter(
     tags = ['User Interface']
@@ -17,14 +18,27 @@ BASE_PATH = Path(__file__).resolve().parent
 TEMPLATES = Jinja2Templates(directory=str(BASE_PATH / "../templates"))
 
 
-@router.get("/")
-def home(request: Request, response_model=HTMLResponse):
-    return TEMPLATES.TemplateResponse("home/index.html", {"request" : request})
+@router.get("/", status_code=status.HTTP_200_OK)
+async def home(request: Request, response_model=HTMLResponse):
 
-@router.get("/login")
-def signin(request: Request, response_model=HTMLResponse):
+    token = request.headers.get('Authorization')
+
+    if (token):
+        return TEMPLATES.TemplateResponse("home/index.html", {"request" : request})
+
+    return RedirectResponse(url=router.url_path_for('signin'))
+
+
+
+@router.get("/login", status_code=status.HTTP_200_OK)
+async def signin(request: Request, response_model=HTMLResponse):
+
+    print('\n\n')
+    print(request.method)
+    print('\n\n')
+
     return TEMPLATES.TemplateResponse("accounts/login.html", {"request" : request})
 
-@router.get("/register")
-def register(request: Request, response_model=HTMLResponse):
+@router.get("/register", status_code=status.HTTP_200_OK)
+async def register(request: Request, response_model=HTMLResponse):
     return TEMPLATES.TemplateResponse("accounts/register.html", {"request" : request})
