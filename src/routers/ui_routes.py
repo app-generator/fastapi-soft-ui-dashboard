06bdfdadcd2285eb67e9329acd51b7d2030633a4
barreraalexander <barreraalexander93@gmail.com>
2022-10-11 08:02:44 -0400

    updating register route to work better with fastapi scheme

--- src/routers/ui_routes.py
+++ src/routers/ui_routes.py
@@ -37,6 +37,7 @@ async def home(request: Request, response_model=HTMLResponse):
 async def signin(request: Request, response_model=HTMLResponse):
     return TEMPLATES.TemplateResponse("accounts/login.html", {"request" : request})
 
+
 @router.post("/login", status_code=status.HTTP_200_OK)
 async def signin(request: Request):
     form = await request.form()
@@ -69,11 +70,36 @@ async def register(request: Request, response_model=HTMLResponse):
 
 @router.post("/register", status_code=status.HTTP_200_OK)
 async def register(request: Request, response_model=HTMLResponse):
-    data = await request.json()
+    form = await request.form()
+    form = form._dict
+    form.pop('register')
+
+    new_user = models.User(**form)
+
+    base_url = request.base_url
+    login_url = app.user_router.url_path_for('create_user')
+    request_url = base_url.__str__() + login_url.__str__()[1:]
+    http3client = http3.AsyncClient()
+    response = await http3client.post(request_url, data=form)
+
+    if (response.status_code==200):
+        data = response.json()
+        token = data['access_token']
+
+        redirect = RedirectResponse(url=router.url_path_for('home'))
+        redirect.status_code = 302
+        redirect.set_cookie('Authorization', f'Bearer {token}')
+        return redirect
+
+
+
     print ('\n\n')
-    print (data)
+    print(new_user.id)
+    print(new_user.email)
+    # print (form)
     print ('\n\n')
 
 
 
     return TEMPLATES.TemplateResponse("accounts/register.html", {"request" : request})
+
