d3bb1a1fb4b9ad3f1319d93cfdef59e64af2147e
barreraalexander <barreraalexander93@gmail.com>
2022-10-10 20:01:42 -0400

    update to ui routes

--- src/routers/ui_routes.py
+++ src/routers/ui_routes.py
@@ -6,7 +6,6 @@ from fastapi.templating import Jinja2Templates
 from pathlib import Path
 import http3
 
-
 import src.oauth2 as oauth2
 from src.config import Settings
 from src import models
@@ -24,12 +23,13 @@ TEMPLATES = Jinja2Templates(directory=str(BASE_PATH / "../templates"))
 @router.get("/", status_code=status.HTTP_200_OK)
 async def home(request: Request, response_model=HTMLResponse):
 
-    print ('\n\n\n')
-    print ('HOME HEADERS')
-    print (request.headers)
-    print ('\n\n\n')
+    auth_token  = request.cookies.get('Authorization')
+    if (auth_token):
+        # verify = oauth2.get_current_user(auth_token)
+        return TEMPLATES.TemplateResponse("home/index.html", {"request" : request})
 
-    return TEMPLATES.TemplateResponse("home/index.html", {"request" : request})
+    return RedirectResponse(router.url_path_for('signin'))    
+
 
 
 
@@ -45,7 +45,9 @@ async def signin(request: Request):
     
     base_url = request.base_url
     login_url = app.auth_router.url_path_for('login')
+    home_url = app.ui_router.url_path_for('home')
     request_url = base_url.__str__() + login_url.__str__()[1:]
+    home_url = base_url.__str__() + home_url.__str__()[1:]
 
     http3client = http3.AsyncClient()
     response = await http3client.post(request_url, data=form)
@@ -53,18 +55,23 @@ async def signin(request: Request):
     if (response.status_code==200):
         data = response.json()
         token = data['access_token']
-        headers = {
-            'Authorization': f'Bearer {token}',
-        }
-        print (headers)
-        # print (headers)
-        redirect = RedirectResponse(url='/')
+
+        redirect = RedirectResponse(url=router.url_path_for('home'))
         redirect.status_code = 302
-        redirect.headers.append('authorization', f'Bearer {token}')
+        # redirect.delete_cookie('Authorization')
+        redirect.set_cookie('Authorization', f'Bearer {token}')
+        # redirect._headers = headers
+
         return redirect
 
     return TEMPLATES.TemplateResponse("accounts/login.html", {"request" : request})
 
+
 @router.get("/register", status_code=status.HTTP_200_OK)
+async def register(request: Request, response_model=HTMLResponse):
+    return TEMPLATES.TemplateResponse("accounts/register.html", {"request" : request})
+
+
+@router.post("/register", status_code=status.HTTP_200_OK)
 async def register(request: Request, response_model=HTMLResponse):
     return TEMPLATES.TemplateResponse("accounts/register.html", {"request" : request})
