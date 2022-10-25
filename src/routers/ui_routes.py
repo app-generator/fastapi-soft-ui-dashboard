48863a4ea8db4723ec45c75e8f0653c50f27e235
barreraalexander <barreraalexander93@gmail.com>
2022-10-25 16:17:06 -0400

    updated error messages

--- src/routers/ui_routes.py
+++ src/routers/ui_routes.py
@@ -55,7 +55,21 @@ async def signin(request: Request):
         redirect.set_cookie('Authorization', f'Bearer {token}')
         return redirect
 
-    return TEMPLATES.TemplateResponse("accounts/login.html", {"request" : request, "msg" : 'Wrong user or password'})
+    if (response.status_code==500):
+        msg = 'Issue with the Server'
+
+    get_users_url = app.user_router.url_path_for('get_users')
+    users_request_url = base_url.__str__() + get_users_url.__str__()[1:]
+    http3client2 = http3.AsyncClient()
+    test_response = await http3client2.get(users_request_url)
+    test_users = test_response.json()
+    msg = 'Wrong User or Password'
+    for test_user in test_users:
+        print (test_user)
+        if form['username']==test_user['email']:
+            msg = 'Password is Wrong'
+        
+    return TEMPLATES.TemplateResponse("accounts/login.html", {"request" : request, "msg" : msg})
 
 
 @router.get("/register", status_code=status.HTTP_200_OK)
@@ -88,7 +102,7 @@ async def register(request: Request, response_model=HTMLResponse):
     if (response.status_code==500):
         msg = 'Issue with the Server'
 
-    get_users_url = app.user_router.url_path_for('create_user')
+    get_users_url = app.user_router.url_path_for('get_users')
     users_request_url = base_url.__str__() + get_users_url.__str__()[1:]
     http3client2 = http3.AsyncClient()
     test_response = await http3client2.get(users_request_url)
